7d7a180bbfca93bdcf59172c169d726691048148
ZeltNamizake <dandriyasyahdi2006@gmail.com>
2026-04-10 09:15:35 +0700

    feat: improve task system, add view layer, and add features EditDoneTime

--- main.go
+++ main.go
@@ -17,8 +17,9 @@ func main() {
 2. Done Task
 3. Undone Task
 4. Delete Task
-5. Show Tasks by Date
-6. Exit
+5. Edit Done Time
+6. Show Tasks by Date
+0. Back/Exit
 Select: `
 
 	for {
@@ -27,11 +28,21 @@ Select: `
 			fmt.Println("[ERR] - ListTasks:", err)
 		}
 
-		selected := handler.AskRequired(menu)
+		selected, ok := handler.AskRequired(menu)
+		if !ok {
+			fmt.Println("[INFO] - Exit")
+			return
+		}
+
 		switch selected {
 
 		case "1":
-			titleTask := handler.AskRequired("Title Task: ")
+			titleTask, ok := handler.AskRequired("Title Task: ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
+
 			if err := handler.AddTask(titleTask); err != nil {
 				fmt.Println("[ERR] - AddTask:", err)
 			} else {
@@ -39,7 +50,11 @@ Select: `
 			}
 
 		case "2":
-			index := handler.AskIntRequired("Task Number Done: ")
+			index, ok := handler.AskIntRequired("Task Number Done: ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
 
 			if err := handler.DoneTask(index); err != nil {
 				fmt.Println("[ERR] - DoneTask:", err)
@@ -48,7 +63,11 @@ Select: `
 			}
 
 		case "3":
-			index := handler.AskIntRequired("Task Number Undone: ")
+			index, ok := handler.AskIntRequired("Task Number Undone: ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
 
 			if err := handler.UndoneTask(index); err != nil {
 				fmt.Println("[ERR] - UndoneTask:", err)
@@ -57,7 +76,11 @@ Select: `
 			}
 
 		case "4":
-			index := handler.AskIntRequired("Task Number Delete: ")
+			index, ok := handler.AskIntRequired("Task Number Delete: ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
 
 			if err := handler.DeleteTask(index); err != nil {
 				fmt.Println("[ERR] - DeleteTask:", err)
@@ -66,17 +89,42 @@ Select: `
 			}
 
 		case "5":
-			date := handler.AskRequired("Date (YYYY-MM-DD): ")
+			index, ok := handler.AskIntRequired("Task Number Markdone: ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
+
+			input, ok := handler.AskRequired("New Done Time (HH:MM): ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
+
+			time, err := handler.ValidateTime(input)
+			if err != nil {
+				fmt.Println("[ERR] -", err)
+				continue
+			}
+
+			if err := handler.EditDoneTime(index, time); err != nil {
+				fmt.Println("[ERR] - EditDoneTime:", err)
+			} else {
+				fmt.Println("[INFO] - Done time update:", time)
+			}
+
+		case "6":
+			date, ok := handler.AskRequired("Date (YYYY-MM-DD): ")
+			if !ok {
+				fmt.Println("[INFO] - Cancelled")
+				continue
+			}
 
 			if err := handler.ListTasksByDate(date); err != nil {
 				fmt.Println("[ERR] - ListTasksByDate:", err)
 			} else {
 				handler.Ask("\n[INFO] - Press enter to continue...")
 			}
-
-		case "6":
-			fmt.Println("Bye!")
-			return
 
 		default:
 			fmt.Println("[ERR] - Invalid choice")
